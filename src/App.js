cb968e4c4db0ce31e5ea3ad6c5679fbd077cd4a9
kensiiwasaki <kensiiwasaki@gmail.com>
2021-07-22 16:40:45 +0900

    node_modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useEffect,useState } from 'react';
+import { useState } from 'react';
 import { List } from "./List";
 import { Form } from "./Form";
 import { getLanguages } from "./const/languages";
